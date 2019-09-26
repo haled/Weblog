97b5199f7d954a1eb359dd58b93dd1c1160fe7b4
Darren Hale <dhale@clearent.com>
2019-09-26 13:57:01 -0500

    Building out Tour of Heroes.

--- TourOfHeroes/UISpike/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/UISpike/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +9,11 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+    constructor(private heroService: HeroService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.getHeroes();
+    }
 
     selectedHero: Hero;
 
@@ -20,5 +21,10 @@ export class HeroesComponent implements OnInit {
         this.selectedHero = hero;
     }
 
-    heroes = HEROES;
+    heroes: Hero[];
+
+    getHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
 }
